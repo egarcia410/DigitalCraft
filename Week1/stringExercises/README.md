54772053a33ddecc18c789fb0d1f50dc36b0bb9e
egarcia410 <egarcia410@gmail.com>
2017-09-12 15:04:07 -0500

    Create: stringExercises file

---
+++ Week1/stringExercises/README.md
@@ -0,0 +1,42 @@
+# String Exercises
+
+### 1. Uppercase a String
+
+Given a string, print the string uppercased.
+
+### 2. Capitalize a String
+
+Given a string, print the string capitalized.
+
+### 3. Reverse a String
+
+Given a string, print the string reversed.
+
+### 4. Leetspeak
+
+Given a paragraph of text as a string, print the paragraph in leetspeak. To translate a string to leetspeak, you need to replace make the following character replacements (treat all input characters as uppercase):
+```
+A => 4
+E => 3
+G => 6
+I => 1
+O => 0
+S => 5
+T => 7
+```
+Example: Leet => l337
+
+### 5. Long-long Vowels
+
+Given a word as a string, print the result of extending any long vowels to the length of 5. Examples:
+```
+Good => Goooood
+Cheese => Cheeeeese
+Man => Man
+Spoon => Spooooon
+```
+### 6. Caesar Cipher
+
+Given a string, print the Caesar Cipher (or ROT13) of that string. What is Caesar Cipher? [Caesar Cipher](http://practicalcryptography.com/ciphers/caesar-cipher/)
+
+Use your solution to decipher the following text: "lbh zhfg hayrnea jung lbh unir yrnearq"
---
+++ Week1/stringExercises/stringExercises.py
@@ -0,0 +1,83 @@
+# 1 Uppercase a String
+# Given a string, print the string uppercased.
+
+string = 'lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+
+# print(string.upper())
+
+# #############################################################################
+# 2 Capitalize a String
+# Given a string, print the string capitalized.
+
+# print(string.capitalize())
+
+# #############################################################################
+# 3 Reverse a String
+# Given a string, print the string reversed.
+
+# print(" ".join(string.split(" ")[::-1]))
+
+# #############################################################################
+# 4 Leetspeak
+# Given a paragraph of text as a string, print the paragraph in leetspeak.
+
+# leetSpeak = {
+#     'A': 4,
+#     'E': 3,
+#     'G': 6,
+#     'I': 1,
+#     'O': 0,
+#     'S': 5,
+#     'T': 7
+# }
+
+# for letter in leetSpeak:
+#     string = string.upper().replace(letter, str(leetSpeak[letter]))
+#
+# print(string.lower())
+
+# #############################################################################
+# 5 Long-long Vowels
+# Given a word as a string, print the result of extending any long vowels to the length of 5
+
+# string = 'Good'
+# count = {
+#     'a': 0,
+#     'e': 0,
+#     'i': 0,
+#     'o': 0,
+#     'u': 0
+# }
+#
+# for letter in string.lower():
+#     if letter in count:
+#         count[letter] += 1
+#
+# print(count)
+# for letter in count:
+#     if count[letter] >= 2:
+#         string = string.replace(letter, letter*4, 1)
+#
+# print(string)
+
+# #############################################################################
+# 6 Caesar Cipher
+# Given a string, print the Caesar Cipher (or ROT13) of that string.
+# What is Caesar Cipher? http://practicalcryptography.com/ciphers/caesar-cipher/
+# Use your solution to decipher the following text: "lbh zhfg hayrnea jung lbh unir yrnearq"
+
+string = "lbh zhfg hayrnea jung lbh unir yrnearq"
+strList = []
+
+# 'z' == 122
+for letter in string:
+    print(ord(letter), letter)
+    if (ord(letter)) == 32:
+        strList.append(chr(ord(letter)))
+        continue
+    if (ord(letter)+1) > 122:
+        strList.append('a')
+    else:
+        strList.append(chr(ord(letter)+1))
+
+print("".join(strList))
