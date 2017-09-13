bf4eb1f9f7cdd038bc530d9444e3244fca583122
egarcia410 <egarcia410@gmail.com>
2017-09-12 21:01:46 -0500

    Update: bonusStringExercise validation

--- Week1/bonusStringExercise/bonusStringExercise.py
+++ Week1/bonusStringExercise/bonusStringExercise.py
@@ -4,29 +4,29 @@ import random
 print('Pick a number between 0 and 100')
 print('The computer will attempt to guess your number')
 
-attempts = 0
+attempts = 1
 low = 0
 high = 100
 num = random.randint(low, high)
 
 while True:
     print('Is your number ' + str(num) + '?')
-    response = input('Yes(Y), Higher(H), or Lower(L)?').lower()
+    response = input('Yes(Y), Higher(H), or Lower(L)? ').lower()
     if (low + 1) == high:
         print('Don\'t lie! Your number is ' + str(num))
         print('Number of attempts: ' + str(attempts))
         break
-    elif response == 'y':
+    elif response == 'y' or response == 'yes':
         print('Your number is ' + str(num))
         print('Number of attempts: ' + str(attempts))
         break
-    elif response == 'h':
+    elif response == 'h' or response == 'higher':
         attempts += 1
-        low = num
+        low = num + 1
         num = int(((high - low) / 2) + low)
-    elif response == 'l':
+    elif response == 'l' or response == 'lower':
         attempts += 1
         high = num
         num = int((high - low) / 2 + low)
     else:
-        print('Please enter valid number')
+        print('Please enter valid response')
