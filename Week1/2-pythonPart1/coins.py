8e5fc1441d2f33b4daa85b69dd576548cd37517e
egarcia410 <egarcia410@gmail.com>
2017-10-03 08:52:44 -0500

    Create: week4 formExercises README

--- Week1/2-pythonPart1/coins.py
+++ Week1/2-pythonPart1/coins.py
@@ -1,21 +1,6 @@
 coins = 0
 print('You have {} coins.'.format(coins))
 
-# res = input('Do you want another? ').lower()
-#
-# while res != 'yes':
-#     res = input('Do you want another? ').lower()
-#
-# while res == 'yes':
-#     coins += 1
-#     print('You have {} coins.'.format(coins))
-#     res = input('Do you want another? ').lower()
-#     while res != 'yes':
-#         if res == 'no':
-#             print('Bye')
-#             break
-#         res = input('Do you want another? ').lower()
-
 while True:
     res = input('Do you want another? ').lower()
     if res == 'yes':
